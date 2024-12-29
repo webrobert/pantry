f1837e76325ae24776aabd77c5ae598b3e6003aa
Robert Wayne <webrobert@gmail.com>
2024-12-28 21:32:40 -0800

    wip

--- app/Http/Livewire/Items/BulkCreate.php
+++ app/Http/Livewire/Items/BulkCreate.php
@@ -11,13 +11,12 @@ class BulkCreate extends Component
 
 	public function save()
 	{
-		$lines = preg_split("/\r\n|\n|\r/", trim($this->items));
-
-		collect($lines)->each( fn($item) => Item::firstOrCreate(['name' => $item]));
+		collect(array_filter(preg_split("/\r\n|\n|\r/", trim($this->items))))
+			->each( fn($item) => Item::firstOrCreate(['name' => $item]));
 
 		$this->reset();
 
-		$this->browserToaster('we sent that one off to the fishes!');
+		$this->browserToaster('Items added!');
 	}
 
     public function render()
